c489ccf83e03ffc228b2fd5f743888e6c64b97ee
Christopher Fouquier <christopher.fouquier@gmail.com>
2015-11-16 14:32:08 +0100

    init

---
+++ README.md
@@ -0,0 +1,80 @@
+# mongoose-seed-plus
+
+mongoose-seed-plus lets you populate and clear MongoDB documents with all the benefits of Mongoose validation
+
+## Basic example
+
+```javascript
+// open => seed.js
+var seeder = require('mongoose-seed-plus');
+
+// Connect to MongoDB via Mongoose
+seeder.connect('mongodb://localhost:27017/mongoose-seed-plus-dev', function() {
+
+    // Start processing...
+    seeder.start(__dirname + '/migrations', [
+        { path: 'models/Product.js', name: 'Product', clear: true },
+        { path: 'models/User.js', name: 'User', clear: true },
+    ]);
+});
+
+
+```
+
+## Data files
+
+Create file `.json` in folder `migrations`.
+
+#### Structure
+
+```
+├── seed.js
+└─┬ migrations
+  ├── users.json
+  ├── products.json
+```
+
+#### Example
+```Javascript
+// open => migrations/users.json
+// Data object containing seed data - documents organized by Model
+{
+    "model": "User",
+    "documents": [
+        {
+            "firstname": "Christopher",
+            "lastname": "Fouquier",
+            "email": "christopher.fouquier@gmail.com",
+            "password": "root",
+            "role": "admin"
+        },
+        {
+            "firstname": "John",
+            "lastname": "Doe",
+            "email": "john.doe@awesome.com",
+            "password": "123"
+        }
+    ]
+}
+```
+
+## Methods
+
+### seeder.connect(db, [callback])
+
+Initializes connection to MongoDB via Mongoose singleton.
+
+### seeder.start(path, models)
+
+```Javascript
+/**
+ * @path (string, required) : path to model
+ * @name (string, required) : name of model
+ * @clear (boolean, required) : clear DB collection
+ */
+__dirname + '/migrations',
+[
+    { path: 'models/User.js', name: 'User', clear: true },
+    { path: 'models/Product.js', name: 'Product', clear: false },
+]
+```
---
+++ index.js
@@ -0,0 +1,169 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ **/
+var _ 			= require('lodash'),
+	fs 			= require('fs'),
+	async 		= require('async'),
+	mongoose 	= require('mongoose'),
+	chalk 		= require('chalk'),
+	path 		= require('path');
+
+function Seeder() {
+	this.connected = false;
+}
+
+Seeder.prototype.connect = function(db, cb) {
+	var _this = this;
+	mongoose.connect(db, function(err) {
+		// Log Error
+		if (err) {
+			console.error(chalk.red('Could not connect to MongoDB!'));
+			console.log(err);
+		} else {
+			_this.connected = true;
+			console.log(chalk.green('Successfully initialized mongoose-seed'));
+			cb();
+		}
+	});
+};
+
+Seeder.prototype.start = function(path, models) {
+	var self = this;
+	this.loadModels(models);
+	this.clearModels(models, function() {
+		self.readData(path, models, function(data) {
+			self.populateModels(data);
+		});
+	});
+};
+
+Seeder.prototype.readData = function(path, models, cb) {
+	var data = [];
+	var migrations_path = path;
+	var files = fs.readdirSync(migrations_path);
+	var count = files.length;
+	files.forEach(function (file, i) {
+	    if (~file.indexOf('.json')) {
+	        var model = require(migrations_path + '/' + file);
+	        data.push(model);
+	    }
+	    if (count === (i + 1)) {
+	    	cb(data);
+	    }
+	});
+};
+
+Seeder.prototype.loadModels = function(models) {
+	models.forEach(function(model) {
+		require(path.resolve(model.path));
+	});
+};
+
+Seeder.prototype.invalidModelCheck = function(models, cb) {
+	var invalidModels = [];
+
+	models.forEach(function(model) {
+		if(_.indexOf(mongoose.modelNames(), model) === -1) {
+			invalidModels.push(model);
+		}
+	});
+
+	if (invalidModels.length) {
+		cb(new Error('Models not registered in Mongoose: ' + invalidModels));
+	} else {
+		cb();
+	}
+};
+
+Seeder.prototype.clearModels = function(models, cb) {
+	if(!this.connected) {
+		return new Error('Not connected to db, exiting function');
+	}
+
+	var modelNames = [];
+
+	// Convert to array if not already
+	if (Array.isArray(models)) {
+		models.forEach(function(model) {
+			modelNames.push(model.name);
+		});
+	} else {
+		console.error(chalk.red('Error: Invalid model type'));
+		return;
+	}
+
+	// Confirm that all Models have been registered in Mongoose
+	var invalidModels = this.invalidModelCheck(modelNames, function(err) {
+		if (err) {
+			console.error(chalk.red('Error: ' + err.message));
+			return;
+		}
+
+		// Clear each model
+		async.each(modelNames, function(modelName, done) {
+			var Model = mongoose.model(modelName);
+			Model.remove({}, function(err) {
+				if (err) {
+					console.error(chalk.red('Error: ' + err.message));
+					return;
+				}
+				console.log(modelName + 's collection cleared');
+				done();
+			});
+		}, function(err) {
+		// Final async callback
+			if(err) { return; }
+			cb();
+		});
+	});
+};
+
+Seeder.prototype.populateModels = function(seedData) {
+	if(!this.connected) {
+		return new Error('Not connected to db, exiting function');
+	}
+
+	var modelNames = _.unique(_.pluck(seedData,'model'));
+
+	// Confirm that all Models have been registered in Mongoose
+	var invalidModels = this.invalidModelCheck(modelNames, function(err) {
+		if (err) {
+			console.error(chalk.red('Error: ' + err.message));
+			return;
+		}
+
+		// Get a count of all the documents so we can close the connection when its done
+		var count = 0;
+		for (var i = 0; i < seedData.length; i++) {
+			count += seedData[i].documents.length;
+		}
+
+		// Populate each model
+		seedData.forEach(function(entry, i) {
+			var Model = mongoose.model(entry.model);
+			entry.documents.forEach(function(document, j) {
+				Model.create(document, function(err) {
+					if (err) {
+						console.error(chalk.red('Error creating document [' + j + '] of ' + entry.model + ' model'));
+						console.error(chalk.red('Error: ' + err.message));
+						return;
+					}
+					console.log('Successfully created document [' + j + '] of ' + entry.model + ' model');
+
+					// decrement count and close connection if all done
+					count--;
+					if (count === 0) {
+						mongoose.connection.close();
+						console.log(chalk.green('Successfully populate mongoose-seed'));
+						process.exit();
+					}
+				});
+			});
+		});
+
+	});
+};
+
+module.exports = new Seeder();
